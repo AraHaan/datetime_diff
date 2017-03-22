c2fee20bfdc933ad1e54ececd3ae75606bb09b52
AraHaan <seandhunt_7@yahoo.com>
2017-03-22 14:47:51 -0500

    Added tests for months.

--- tests/test_datetime_diff.py
+++ tests/test_datetime_diff.py
@@ -11,13 +11,25 @@ import datetime_diff
 def test_years():
     now = datetime.datetime.now()
     olddate = datetime.datetime(now.year - 1, 12, 3, 3, 34, 57, 281000)
-    datetime_diff.datetime_diff(olddate, now)
+    assert datetime_diff.datetime_diff(olddate, now) == "1 year ago."
 
 
 def test_multiple_years():
     now = datetime.datetime.now()
     olddate = datetime.datetime(now.year - 2, 12, 3, 3, 34, 57, 281000)
-    datetime_diff.datetime_diff(olddate, now)
+    assert datetime_diff.datetime_diff(olddate, now)  == "2 years ago."
+
+
+def test_months():
+    now = datetime.datetime.now()
+    olddate = datetime.datetime(now.year, now.month - 1, now.day, now.hour, now.minute, now.second)
+    assert datetime_diff.datetime_diff(olddate, now) == "1 month ago."
+
+
+def test_multiple_months():
+    now = datetime.datetime.now()
+    olddate = datetime.datetime(now.year, now.month - 2, now.day, now.hour, now.minute, now.second)
+    assert datetime_diff.datetime_diff(olddate, now)  == "2 months ago."
 
 
 def test_datetime_diff():
